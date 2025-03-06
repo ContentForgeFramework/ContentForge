404aef0fe6439015eb067dd118beed68d924a381
Content Forge <ContentForge.org@hotmail.com>
2025-03-07 02:38:04 +0800

    update contentforge pkg path

---
+++ vendor/contentforge/__about__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ====================================================================================================================================================
+# 
+# TODO: Short description for file
+# 
+# TODO: Long description for file (if any)...
+# 
+# @filename   __about__.py
+# @path       RELATIVE_PATH
+# @project    PROJECT_NAME
+# @encoding   utf-8
+# 
+# @product    PyCharm
+# @author     USER
+# @email      EMAIL
+# @time       2025/3/7 02:37
+# 
+# @version    git
+# @record     CURRENT_USER_NAME Create file.
+#             CURRENT_USER_NAME Update header comment.
+#             CURRENT_USER_NAME description
+# 
+# @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+#
+# @copyright  Copyright (c) 2025. All rights reserved.
+#
+#             This software, including its code, documentation, and related materials,
+#             is protected by copyright law and international treaties.
+#             Unauthorized use, reproduction, or distribution of any part of this software is prohibited.
+#
+#             ContentForge is a trademark of ContentForge.org.
+#             All other trademarks and registered trademarks are the property of their respective owners.
+#
+#             For permissions, please contact ContentForge.org@hotmail.com.
+# ====================================================================================================================================================
+from __future__ import print_function, unicode_literals
+
+# =============================================================================================================
+# Standard Python Imports
+# =============================================================================================================
+import os
+import sys
+
+
+# =============================================================================================================
+# Script Execution
+# =============================================================================================================
+if __name__ == '__main__':
+    pass
